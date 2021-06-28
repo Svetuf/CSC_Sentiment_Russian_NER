a3b9beebe52feae3b126d048861b35bb9f9c45a3
margaritatsobenko <margaritatsobenko@yandex.ru>
2021-06-28 14:23:56 +0300

    update functions

--- Utils/functions.py
+++ Utils/functions.py
@@ -1,22 +1,25 @@
-import re
 import collections
-from nltk.probability import FreqDist
 import nltk
 import numpy as np
+import re
+import spacy
+
 from matplotlib import pyplot as plt
 from nltk import ngrams
+from nltk.probability import FreqDist
 from pymystem3 import Mystem
 from spacy.lang.ru import Russian
-import spacy
+
 
 def read_file(file):
     f = open(file, 'r')
-    Lines = f.readlines()
+    lines = f.readlines()
     ans = []
-    for line in Lines:
+    for line in lines:
         if '<column name=\"text\">' in line:
             ans.append(line.strip().replace('<column name=\"text\">', '').replace('</column>', ''))
     return ans
+
 
 def regul(s, dat):
     ans = []
@@ -30,6 +33,7 @@ def regul(s, dat):
         except:
             pass
     return ans
+
 
 def create_hist(x, y, title, xlabel, ylabel):
     plt.hist(x, y)
@@ -50,20 +54,21 @@ def create_boxplot(s):
 def get_ngrams(n, data, best=True, worst=False, top=10):
     n_grams = []
     for i in data:
-        cur = ngrams(i.split(), n)
-        n_grams += [a for a in cur]
-    NgramFreq = collections.Counter(n_grams)
+        n_grams += [a for a in ngrams(i.split(), n)]
+
+    ngram_freq = collections.Counter(n_grams)
     if best == True:
         print(f'Лучшие {top} {n}-грамм')
-        print(NgramFreq.most_common(10))
+        print(ngram_freq.most_common(10))
     if worst == True:
         print(f'Худшие {top} {n}-грамм')
-        print(NgramFreq.most_common()[::-1][:10])
+        print(ngram_freq.most_common()[::-1][:10])
+
 
 def get_frequent_words(data, top=10):
     split_it = data.split()
-    Counter = collections.Counter(split_it)
-    return Counter.most_common(top)
+    counter = collections.Counter(split_it)
+    return counter.most_common(top)
 
 
 def get_lemmatize(text):
@@ -81,8 +86,8 @@ def get_freq_pos(data, file):
     for token in doc:
         s.append((token.text, token.pos_))
 
+    val_1 = collections.Counter(y for (_, y) in s)
 
-    val_1=collections.Counter([y for (x,y) in s])
     most_occur_tag = val_1.most_common()
     return most_occur_tag
 
@@ -92,10 +97,6 @@ def get_pos(data, file):
     nlp = spacy.load(file)
 
     for s in data:
-        text = nlp(s)
-        cur = []
-        for t in text:
-            cur.append(t.pos_)
-        ans.append(' '.join(cur))
+        ans.append(' '.join(t.pos_ for t in nlp(s)))
 
     return ans
